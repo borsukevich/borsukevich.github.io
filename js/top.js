102f1e5e9f5d01b29271d6df6cc9db3d0041bc35
Misha Borsukevich <borsukevich.misha@gmail.com>
2017-03-12 17:36:25 +0200

    Lingualand

---
+++ js/top.js
@@ -0,0 +1,1 @@
+var top_show=700,delay=1e3;$(document).ready(function(){$(window).scroll(function(){$(this).scrollTop()>top_show?$("#top").fadeIn():$("#top").fadeOut()}),$("#top").click(function(){$("body, html").animate({scrollTop:0},delay)})});
