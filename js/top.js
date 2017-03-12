50142a43880c7b2be00c216e9d5fc2faed0905b8
Misha Borsukevich <borsukevich.misha@gmail.com>
2017-03-12 17:51:48 +0200

    delete min

--- js/top.js
+++ js/top.js
@@ -1,1 +1,11 @@
-var top_show=700,delay=1e3;$(document).ready(function(){$(window).scroll(function(){$(this).scrollTop()>top_show?$("#top").fadeIn():$("#top").fadeOut()}),$("#top").click(function(){$("body, html").animate({scrollTop:0},delay)})});
+var top_show = 700, delay = 1e3;
+$(document).ready(function() {
+	$(window).scroll(function() {
+		$(this).scrollTop() > top_show ? $("#top").fadeIn() : $("#top").fadeOut()
+	}),
+	$("#top").click(function() {
+		$("body, html").animate({
+			scrollTop: 0
+		}, delay)
+	})
+});
